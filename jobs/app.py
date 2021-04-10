561571dce04b41aadd2c1a98acbb140c49200eda
LeonardMeza <74522691+LeonardMeza@users.noreply.github.com>
2021-04-09 19:16:06 -0700

    Completed module

--- jobs/app.py
+++ jobs/app.py
@@ -1,6 +1,6 @@
 import sqlite3
-from flask import Flask, g
-from flask import render_template
+import datetime
+from flask import Flask, render_template, g, request, redirect, url_for
 PATH = 'db/jobs.sqlite'
 
 
@@ -22,9 +22,8 @@ def execute_sql(sql, values= (), commit= False, single= False):
         results = connection.commit()
     else:
         results = cursor.fetchone() if single else cursor.fetchall()
-        cursor.close()
-        return results
-
+    cursor.close()
+    return results
 
 @app.teardown_appcontext
 def close_connection(exception):
@@ -45,10 +44,23 @@ def job(job_id):
     job = execute_sql('SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id WHERE job.id = ?', [job_id], single=True)
     return render_template('job.html', job=job)
 
-
 @app.route('/employer/<employer_id>')
 def employer(employer_id):
-    employer = execute_sql( 'SELECT * FROM employer WHERE id=?', [employer_id], single=True)
+    employer = execute_sql('SELECT * FROM employer WHERE id=?', [employer_id], single=True)
     jobs = execute_sql('SELECT job.id, job.title, job.description, job.salary FROM job JOIN employer ON employer.id = job.employer_id WHERE employer.id = ?', [employer_id])
     reviews = execute_sql('SELECT review, rating, title, date, status FROM review JOIN employer ON employer.id = review.employer_id WHERE employer.id = ?', [employer_id])
     return render_template('employer.html', employer=employer, jobs=jobs, reviews=reviews)
+
+@app.route('/employer/<employer_id>/review', methods=('GET', 'POST'))
+def review(employer_id):
+    if request.method == 'POST':
+        review = request.form['review']
+        rating = request.form['rating']
+        title = request.form['title']
+        status = request.form['status']
+        date = datetime.datetime.now().strftime("%m/%d/%Y")
+
+        execute_sql('INSERT INTO review (review, rating, title, date, status, employer_id) VALUES (?, ?, ?, ?, ?, ?)', (review, rating, title, date, status, employer_id), commit=True)
+        return redirect(url_for('employer', employer_id=employer_id))
+
+    return render_template('review.html', employer_id=employer_id)
